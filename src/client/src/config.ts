df8435e18ce556122b70b446be10d2b310c215a2
Asaf Cohen <asaf@permit.io>
2022-07-05 13:56:32 +0300

    added .env wiring

--- src/client/src/config.ts
+++ src/client/src/config.ts
@@ -19,12 +19,21 @@ const Config: ConfigOptions = {
       "http://localhost:8008",
   },
   auth0: {
-    domain: "acalla-demoapp.us.auth0.com",
-    clientId: "w4SUOtl4HacGNbFvEiqY3zBUdDJr128x",
-    audience: "https://demoapi.acalla.com/v1/",
+    domain:
+      process.env.AUTH0_DOMAIN ||
+      window?._env_?.AUTH0_DOMAIN ||
+      "acalla-demoapp.us.auth0.com",
+    clientId:
+      process.env.AUTH0_CLIENT_ID ||
+      window?._env_?.AUTH0_CLIENT_ID ||
+      "w4SUOtl4HacGNbFvEiqY3zBUdDJr128x",
+    audience:
+      process.env.AUTH0_AUDIENCE ||
+      window?._env_?.AUTH0_AUDIENCE ||
+      "https://demoapi.acalla.com/v1/",
   },
   authentication: {
-    provider: window?._env_?.AUTHENTICATION_PROVIDER || "auth0",
+    provider: "auth0",
   },
   authorization: {
     embedUrl: window?._env_?.AUTHZ_EMBED_URL || "http://localhost:3000",
